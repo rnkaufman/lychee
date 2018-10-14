d1fdb984b6b0e01ddaeea2f9042cb51e7c529157
rnkaufman <misskaufman@gmail.com>
2018-10-13 21:48:05 -0700

    add .env, start adding basis for ingredient finder

---
+++ usda_api/ingredient_finder.rb
@@ -0,0 +1,38 @@
+require "dotenv"
+require "httparty"
+require "pry"
+
+class IngredientFinder
+  attr_reader :item
+
+  Dotenv.load("../.env")
+  USDA_API_KEY = ENV['USDA_API_KEY']
+
+  def initialize(item)
+    @item = item 
+    list_ingredients
+  end
+
+  def list_ingredients
+    ndb_item_number = fetch_ndb_item_number
+    if ndb_item_number
+      item_endpoint = "https://api.nal.usda.gov/ndb/reports/?ndbno=#{ndb_item_number}&type=b&format=json&api_key=#{USDA_API_KEY}"
+      response = HTTParty.get(item_endpoint)
+      puts response["report"]["food"]["ing"]["desc"]
+    end
+  end
+
+  private
+  def fetch_ndb_item_number
+    search_endpoint = "https://api.nal.usda.gov/ndb/search/?format=json&q=#{@item}&sort=n&max=25&offset=0&api_key=#{USDA_API_KEY}"
+    response = HTTParty.get(search_endpoint)
+
+    begin    
+      response["list"]["item"][0]["ndbno"]
+    rescue
+      raise "No items were found"
+    end
+  end
+end
+
+IngredientFinder.new(ARGV[0])
