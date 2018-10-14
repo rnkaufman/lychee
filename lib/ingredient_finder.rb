78a84ad0b34cc9a5fccbe29df9116e754960d23d
rnkaufman <misskaufman@gmail.com>
2018-10-13 23:04:05 -0700

    Add vcr, spec for listing ingredients, move ingredients file to lib

--- lib/ingredient_finder.rb
+++ lib/ingredient_finder.rb
@@ -4,7 +4,7 @@ require "httparty"
 class IngredientFinder
   attr_reader :item
 
-  Dotenv.load("../.env")
+  Dotenv.load(".env")
   USDA_API_KEY = ENV['USDA_API_KEY']
 
   def initialize(item)
---
+++ spec/ingredient_finder_spec.rb
@@ -0,0 +1,13 @@
+require 'vcr'
+require 'ingredient_finder'
+
+RSpec.describe IngredientFinder do
+  subject { IngredientFinder.new("Trader Joe's Acacia Honey") }
+  describe "#list_ingredients" do
+    context "with discovered item", :vcr do
+      it "returns the ingredients", vcr: {cassette_name: :usda_get_item} do
+        expect(subject.list_ingredients).to eq("HONEY, BLACK SUMMER TRUFFLE PIECES, NATURAL MUSHROOM FLAVOR.")
+      end
+    end 
+  end
+end
