b95a1567afa223483be30bbd3fb27afabcf893e5
rnkaufman <misskaufman@gmail.com>
2018-10-13 23:07:38 -0700

    Clean up vcr files

--- spec/ingredient_finder_spec.rb
+++ spec/ingredient_finder_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe IngredientFinder do
   subject { IngredientFinder.new("Trader Joe's Acacia Honey") }
   describe "#list_ingredients" do
     context "with discovered item", :vcr do
-      it "returns the ingredients", vcr: {cassette_name: :usda_get_item} do
+      it "returns the ingredients" do
         expect(subject.list_ingredients).to eq("HONEY, BLACK SUMMER TRUFFLE PIECES, NATURAL MUSHROOM FLAVOR.")
       end
     end 
