8c808b8a914078f02b68e7d174facc5904b6c551
rnkaufman <misskaufman@gmail.com>
2018-10-15 20:37:08 -0700

    Change from titles to upc numbers

--- lib/ingredient_finder.rb
+++ lib/ingredient_finder.rb
@@ -2,13 +2,13 @@ require "dotenv"
 require "httparty"
 
 class IngredientFinder
-  attr_reader :item
+  attr_reader :upc_number
 
   Dotenv.load(".env")
   USDA_API_KEY = ENV['USDA_API_KEY']
 
-  def initialize(item)
-    @item = item 
+  def initialize(upc_number)
+    @upc_number = upc_number
   end
 
   def list_ingredients
@@ -19,20 +19,20 @@ class IngredientFinder
       begin
         response["report"]["food"]["ing"]["desc"]
       rescue
-        raise "No ingredients found for item '#{@item}'"
+        raise "No ingredients found for upc_number '#{@upc_number}'"
       end
     end
   end
 
   private
   def fetch_ndb_item_number
-    search_endpoint = "https://api.nal.usda.gov/ndb/search/?format=json&q=#{@item}&sort=n&max=25&offset=0&api_key=#{USDA_API_KEY}"
+    search_endpoint = "https://api.nal.usda.gov/ndb/search/?format=json&q=#{@upc_number}&sort=n&max=25&offset=0&api_key=#{USDA_API_KEY}"
     response = HTTParty.get(search_endpoint)
 
     begin    
       response["list"]["item"][0]["ndbno"]
     rescue
-      raise "No items matching '#{@item}' were found"
+      raise "No items matching upc_number '#{@upc_number}' were found"
     end
   end
 end
